77224ade9da3dd1270eee20b35f0cf737bf10ddb
Rajat Soni <98265341+rajat-sonigit@users.noreply.github.com>
2025-01-13 22:55:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/socketmode"
 )
+
 func printCommandEvents(analyticChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticChannel {
 		fmt.Println("Command Events")
